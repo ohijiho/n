2647dc3ef80070736b7fac16ea3ae469c7f3ae5a
Jiho Park <ohijiho@gmail.com>
2024-05-24 02:21:14 +0900

    add missing file

---
+++ n_helper.py
@@ -0,0 +1,48 @@
+from itertools import count
+
+
+def fetch_problem(n):
+    ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+    url = f'https://www.acmicpc.net/problem/{n}'
+    import requests
+    res = requests.get(url, headers={'User-Agent': ua})
+    if not res.ok:
+        raise Error('fetching {url} failed: {res.status_code} {res.reason}')
+    return res.text
+
+
+def extract_sample_ios(html):
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(html, 'html.parser')
+    res = []
+    for i in count(1):
+        e = soup.find(id=f'sample-input-{i}')
+        if e is None:
+            break
+        res.append((e.text, soup.find(id=f'sample-output-{i}').text))
+    return res
+
+
+def make_tcs_cat(n, in_file, out_file):
+    ios = extract_sample_ios(fetch_problem(n))
+    if not ios:
+        return
+    sin, sout = zip(*ios)
+    in_file.write(''.join(sin))
+    out_file.write(''.join(sout))
+
+
+def cmd_make_tcs_cat(n, in_file, out_file):
+    with open(in_file, "w") as in_file, open(out_file, "w") as out_file:
+        make_tcs_cat(n, in_file, out_file)
+
+
+def main(cmd, *argv):
+    cmds = {
+        'make_tcs_cat': cmd_make_tcs_cat,
+    }
+    cmds[cmd](*argv)
+
+
+import sys
+main(*sys.argv[1:])
